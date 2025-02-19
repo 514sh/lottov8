7132279004f756fac83bb4d9b1cbfc3a9d87558e
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 19:23:20 +0800

    chore: wrong commit - delete lines

--- lotto/file_reader.py
+++ lotto/file_reader.py
@@ -2,5 +2,3 @@ def read_input(base_dir,date_str,owner_name):
     with open(f"{base_dir}/input/{date_str}_{owner_name}.txt", encoding="utf-8") as input_file:
         return input_file.read()
     
-def validate_line(line):
-    
