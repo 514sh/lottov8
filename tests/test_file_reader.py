23a53a68c82b18a0333510355d140612c1501a9e
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 18:12:13 +0800

    t: i can parse output from input file to numpy array

--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -1,6 +1,11 @@
-from lotto import file_reader
+from lotto import get_lines
+import numpy as np
 
 def test_read_input_file_given_base_dir_date_and_owner_name(input_from_file):
-    # input_contents = file_reader.read_input(base_dir="test_data", date_str="02_19_2025", owner_name="one")
     assert input_from_file == "1 2 3 5\n1 2 4 5"
     
+def test_i_can_parse_output_from_input_file_to_numpy_array(input_from_file):
+    input_lines = get_lines(input_from_file)
+    expected_output = np.array(["1 2 3 5", "1 2 4 5"])
+    assert np.all(input_lines == expected_output) == True
+        
