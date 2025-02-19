47787c0db8093619a13740f262d19628b403151d
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 19:24:05 +0800

    test passed: i can split a str line to numpy array

--- lotto/input_scrubber.py
+++ lotto/input_scrubber.py
@@ -1,5 +1,19 @@
+import re
+
 import numpy as np
 
 def get_lines(input_str):
     return np.array(input_str.split("\n"))
 
+
+
+def validate_line(line, game_draw):
+    entry = split_line(line)
+    if entry.size == 4:
+        return True
+    return False
+
+    
+def split_line(line):
+    entry = re.split(r"[^0-9]", line)
+    return np.array([int(number) for number in entry if number])
--- tests/test_input_scrubber.py
+++ tests/test_input_scrubber.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lotto.input_scrubber import get_lines, validate_line
+from lotto.input_scrubber import get_lines, validate_line, split_line
 
 
 def test_i_can_parse_output_from_input_file_to_numpy_array(input_from_file):
@@ -7,7 +7,12 @@ def test_i_can_parse_output_from_input_file_to_numpy_array(input_from_file):
     expected_output = np.array(["1 2 3 5", "1 2 4 5"])
     assert np.all(input_lines == expected_output) == True
     
-def test_i_can_validate_a_valid_line(valid_input):
-    sample_draw = "42"
-    is_valid = validate_line(valid_input, sample_draw)
-    assert is_valid == True
+def test_i_can_split_a_str_line_to_numpy_array(valid_input):
+    splitted_line = split_line(valid_input)
+    expected_output = np.array([1, 2, 3, 5])
+    assert np.all(splitted_line == expected_output) == True
+    
+# def test_i_can_validate_a_valid_line(valid_input):
+#     sample_draw = 42
+#     is_valid = validate_line(valid_input, sample_draw)
+#     assert is_valid == True
