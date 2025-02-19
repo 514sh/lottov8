03e77a0d11388b90f5e1f4489490bd9f70fe5970
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 18:17:10 +0800

    t passed: i can parse output from input file to numpy array

--- lotto/file_reader.py
+++ lotto/file_reader.py
@@ -1,3 +1,8 @@
+import numpy as np
+
 def read_input(base_dir,date_str,owner_name):
     with open(f"{base_dir}/input/{date_str}_{owner_name}.txt", encoding="utf-8") as input_file:
         return input_file.read()
+    
+def get_lines(input_str):
+    return np.array(input_str.split("\n"))
