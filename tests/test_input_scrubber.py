6545967f562e17ef64eb0969f94980f0fe0e0ce2
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 19:40:23 +0800

    test passed: validate that if a combination exceeds 45 it is not valid in a 6 45 game draw

--- tests/test_input_scrubber.py
+++ tests/test_input_scrubber.py
@@ -32,3 +32,8 @@ def test_i_can_validate_that_if_a_combination_exceeds_42_it_is_not_valid_in_a_6_
     wrong_input_wrong_combination_exceeds_entry_in_6_42_draw):
     is_valid = validate_line(wrong_input_wrong_combination_exceeds_entry_in_6_42_draw, sample_draw_6_42)
     assert is_valid == False
+    
+def test_i_can_validate_that_if_a_combination_exceeds_45_it_is_not_valid_in_a_6_45_game_draw(
+    wrong_input_wrong_combination_exceeds_entry_in_6_45_draw):
+    is_valid = validate_line(wrong_input_wrong_combination_exceeds_entry_in_6_45_draw, sample_draw_6_45)
+    assert is_valid == False
