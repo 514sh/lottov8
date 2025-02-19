a8b50e39f5b7bc03c2db4231ef80f05b4a51627c
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 17:29:18 +0800

    test: refactor input from file as fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,6 @@
 import pytest
+from lotto import file_reader
+
+@pytest.fixture()
+def input_from_file():
+    return file_reader.read_input(base_dir="test_data", date_str="02_19_2025", owner_name="one")
