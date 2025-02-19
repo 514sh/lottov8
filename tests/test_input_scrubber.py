9795414f57ae9c2ecbc890f4e2abeb5c5ddccfe3
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 18:51:32 +0800

    t: test_i_can_validate_a_valid_line

--- tests/test_input_scrubber.py
+++ tests/test_input_scrubber.py
@@ -1,9 +1,12 @@
 import numpy as np
-from lotto.input_scrubber import get_lines
+from lotto.input_scrubber import get_lines, validate_line
 
 
 def test_i_can_parse_output_from_input_file_to_numpy_array(input_from_file):
     input_lines = get_lines(input_from_file)
     expected_output = np.array(["1 2 3 5", "1 2 4 5"])
     assert np.all(input_lines == expected_output) == True
-        
+    
+def test_i_can_validate_a_valid_line(valid_input):
+    is_valid = validate_line(valid_input)
+    assert is_valid == True
