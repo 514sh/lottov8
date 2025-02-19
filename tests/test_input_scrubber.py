70b1faa8b5e767957b199e06743a00ed17ecdb09
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 20:49:17 +0800

    chore removed print statements

--- tests/test_input_scrubber.py
+++ tests/test_input_scrubber.py
@@ -77,9 +77,7 @@ def test_i_can_separate_valid_lines_from_invalid_lines_given_only_valid_lines(sa
     
 def test_i_can_separate_valid_lines_from_invalid_lines_given_only_invalid_lines_in_a_6_42_game_draw(
     sample_input_str_only_wrong):
-    print(sample_input_str_only_wrong)
     invalid_lines = lines_separator(sample_input_str_only_wrong, sample_draw_6_42)[1]
-    print(lines_separator(sample_input_str_only_wrong, sample_draw_6_42)[0])
     expected_output = {
         1: "1 2 3 4",
         2: "1 3 3 5",
