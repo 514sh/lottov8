4a76ce0993bc88df0e40e9cb2e9598879b4fac5d
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 20:26:31 +0800

    test passed: refactor separated valid lines from invalid lines given only valid lines

--- lotto/input_scrubber.py
+++ lotto/input_scrubber.py
@@ -2,8 +2,9 @@ import re
 
 import numpy as np
 
+
 def get_lines(input_str):
-    return np.array(input_str.split("\n"))
+    return input_str.split("\n")
 
 
 def validate_line(line, game_draw):
@@ -20,3 +21,17 @@ def validate_line(line, game_draw):
 def split_line(line):
     entry = re.split(r"[^0-9]", line)
     return np.array([int(number) for number in entry if number])
+
+
+def lines_separator(input_str, game_draw):
+    splitted_lines = get_lines(input_str)
+    valid_lines = {}
+    invalid_lines = {}
+    for index, line in enumerate(splitted_lines):
+        line_number = index + 1
+        is_valid = validate_line(line, game_draw)
+        if is_valid:
+            valid_lines[line_number] = split_line(line)
+        elif not is_valid and is_valid is not None:
+            invalid_lines[line_number] = line
+    return valid_lines, invalid_lines
