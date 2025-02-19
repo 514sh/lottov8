28ec00436a1298b91b92be6895e59c3a05328393
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 18:51:08 +0800

    test: import fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from lotto import file_reader
 
-@pytest.fixture()
+from tests.fixtures import *
+
+@pytest.fixture
 def input_from_file():
     return file_reader.read_input(base_dir="test_data", date_str="02_19_2025", owner_name="one")
