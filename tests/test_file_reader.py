3681af7f5f9920ab69b0630f2a91c41f9869e632
514sh <mark.joseph.balagtas@outlook.com>
2025-02-19 15:29:43 +0800

    t improved(multiple lines in a file) - read input file given base_dir, date and owner name

--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -2,4 +2,4 @@ from lotto import file_reader
 
 def test_read_input_file_given_base_dir_date_and_owner_name():
     input_contents = file_reader.read_input(base_dir="test_data", date_str="02_19_2025", owner_name="one")
-    assert input_contents == "1 2 3 5"
+    assert input_contents == "1 2 3 5\n1 2 4 5"
